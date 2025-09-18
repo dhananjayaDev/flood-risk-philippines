f6c12313d9a76564927f67470a1e19850c097262
dhananjayaDev <d.dhananjaya@outlook.com>
2025-09-18 14:54:53 +0530

    ci: app.py update

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+# from app import create_app, db, socketio
+# from app.models import User
+
+# app = create_app()
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User}
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+
 from app import create_app, db, socketio
 from app.models import User
 
@@ -8,4 +21,8 @@ def make_shell_context():
     return {'db': db, 'User': User}
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    # For local development
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+else:
+    # For production (Render)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
